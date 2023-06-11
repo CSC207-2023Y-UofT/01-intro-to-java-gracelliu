330830f4cf7b39a18b1ad6833c1832de91031470
gracelliu <gracel.liu@mail.utoronto.ca>
2023-06-11 00:10:16 -0400

    deleted all the prints i used for testing.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -81,18 +81,6 @@ public class Basics {
          *    (Relevant reading: 1.8.2. for Loops)
          */
 
-        /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
-         *
-         * e.g. the end result should look like:
-         * Current count: 10
-         * Current count: 9
-         *     ...
-         * Current count: 1
-         * Current count: 0
-         */
 
         for (int i = 10; i >= 0; i = i - 1) {
             System.out.println("Current count: " + i);
@@ -130,13 +118,7 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
-         *                The String methods .split and .charAt may be helpful,
-         *                along with the StringBuilder.append
-         *                You may also assume that to_split always has 7 words
-         *                separated by spaces, so no loops are required, though
-         *                you may use them if you wish.
-         */
+
         StringBuilder ret = new StringBuilder();
 
         String[] temp = to_split.split(" ");
@@ -171,25 +153,6 @@ public class Basics {
             current_sum += arr[i];
         }
 
-
-        System.out.println();
         return current_sum;
     }
-
-
-    /* TODO (Task 5): Complete this method body using a for-loop.
-     *                You can find the length of an array by using the
-     *                .length attribute (e.g. arr.length)
-     *                You can index into arrays as we do in Python
-     *                (e.g. arr[i] gives you the item at index i).
-     */
-    /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
-     *
-     *                You can click on the ongoing action to see more details.
-     *                If the tests don't pass, look at the results and revise
-     *                accordingly.
-     */
 }
